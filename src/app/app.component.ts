8322c64e242c299f09ab83e7bdb39f3bf44bdf9d
DEVY Jerome <jdevy.ext@orange.com>
2018-10-25 10:53:28 +0200

    change l'image .png moins lourde

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from "@angular/service-worker"
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   preserveWhitespaces: false,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  constructor(private swUpdate: SwUpdate) {
+  }
+
+  ngOnInit() {
+
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(event => {
+        this.swUpdate.activateUpdate().then(() => document.location.reload());
+      })
+    }
+  }
+
+}
+
